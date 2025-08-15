cbf3b2f850084cf480474e547be85617e63e6e72
Ching-Yang Tan <jack5542856@gmail.com>
2025-08-15 18:57:36 +0930

    Fix: Add torchvision dependency and remove venv activation

--- run_face_system.sh
+++ run_face_system.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sleep 5
-source "/app/venv/bin/activate"
 #xhost +
 #export DISPLAY=:0
 python3 "/app/face_main.py"
--- run_set_form.sh
+++ run_set_form.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source "/app/venv/bin/activate"
 #export DISPLAY=:0
 python3 "/app/set_form.py"
